cd95797df60c81199d95a5f32cf0dcfc994dc5fd
Ranjith Bhaskaran <ranjithbhaskaran05@gmail.com>
2024-10-01 15:13:44 +0530

    Records varaikum is here

--- website/views.py
+++ website/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
+
 
 # Create your views here.
+def home(request):
+
+    records = Record.objects.all()
+
+    #check if user is logging in 
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        #Authenticate the user
+
+        user = authenticate(request, username=username, password= password)     
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in")
+            return redirect('home')   
+        else:
+            messages.success(request, "There was an error logging in...")
+
+    
+    return render(request, 'home.html', {'records':records})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out...")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login the user after registration
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully registered. Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+
+    # Render the form for both GET requests and invalid POST submissions
+    return render(request, 'register.html', {'form': form})
